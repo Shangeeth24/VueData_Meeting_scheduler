d0cb1a72514ed1eabbc99e87158e2a4954a6bf92
Shangeeth <Shangeeth.s@vuedata.in>
2023-11-01 12:27:56 +0530

    React

--- Controllers/controllers.js
+++ Controllers/controllers.js
@@ -4,23 +4,23 @@ const room = require("../Model/room");
 const employee = require("../Model/employee");
 const data = require("../Service/lookup.js");
 const moment = require("moment");
-
+ 
 exports.insertBooking = async (req, res) => {
   try {
     const { startTime, endTime, roomId, employeeId } = req.body;
     console.log(startTime, endTime, roomId);
     const start = moment(startTime);
     const end = moment(endTime);
-
+ 
     if (end.isBefore(start)) {
       return res.status(400).json({ message: "End time should be after start time" });
     }
-
+ 
     const currentTime = moment();
     if (start.isBefore(currentTime)) {
       return res.status(400).json({ message: "Cannot book time in the past" });
     }
-
+ 
     const overlappingBooking = await booking.findOne({
       $and: [
         { roomId },
@@ -33,7 +33,7 @@ exports.insertBooking = async (req, res) => {
         },
       ],
     });
-
+ 
     if (overlappingBooking) {
       console.log("overlapping exists");
       res.status(400).send({
@@ -41,21 +41,25 @@ exports.insertBooking = async (req, res) => {
       });
       return;
     }
-
+ 
     const duration = moment.duration(end.diff(start)).asHours();
-
-    if (duration > 30) {
+    const minDuration = moment.duration(end.diff(start)).asMinutes();
+ 
+    if (duration > 1) {
       return res.status(400).json({ message: "Meeting duration exceeds 30 hours" });
     }
-
+    if (minDuration < 15) {
+      return res.status(400).json({ message: "Cannot Book Meetings less than 15 Minutes" });
+    }
+ 
     let sequence = await meetingIdGenerate.findOneAndUpdate(
       { meeting: "justForChecking" },
       { $inc: { meetingId: 1 } },
       { new: true }
     );
-
+ 
     let incrementedId;
-
+ 
     if (sequence === null) {
       const firstTimeValue = new meetingIdGenerate({
         meeting: "justForChecking",
@@ -66,24 +70,24 @@ exports.insertBooking = async (req, res) => {
     } else {
       incrementedId = sequence.meetingId;
     }
-
+ 
     let newBookingData = req.body;
     newBookingData.meetingId = incrementedId;
-
+ 
     var newBooking = new booking(newBookingData);
     await newBooking.save();
-
+ 
     res.status(200).send("Data received");
   } catch (error) {
     console.error("Error inserting booking:", error);
     res.status(500).send("Internal Server Error");
   }
 };
-
+ 
 exports.insertBookingView = (req, res) => {
   res.render("booking", { currentPath: "/Booking" });
 };
-
+ 
 exports.roomName = async (req, res) => {
   try {
     const rooms = await room.find({});
@@ -92,7 +96,7 @@ exports.roomName = async (req, res) => {
     res.status(500).send("Internal Server Error");
   }
 };
-
+ 
 exports.employeeName = async (req, res) => {
   try {
     const employees = await employee.find({});
@@ -101,106 +105,38 @@ exports.employeeName = async (req, res) => {
     res.status(500).send("Internal Server Error");
   }
 };
-
+ 
 const today = new Date();
 today.setHours(0, 0, 0, 0);
-
+ 
+ 
 exports.today = async (req, res, next) => {
   try {
     const datas = await data.getBooking();
     const startOfDay = new Date(today);
     const endOfDay = new Date(today);
     endOfDay.setDate(endOfDay.getDate() + 1);
-
+ 
+    const now = new Date();
+ 
     const filteredData = datas.filter((item) => {
       return item.startTime >= startOfDay && item.startTime < endOfDay;
     });
-
+ 
     filteredData.sort((a, b) => a.startTime - b.startTime);
-    
-    if (filteredData.length === 0) {
-      res.render("index", {
-        nav1: "Dashboard",
-        nav2: "Book",
-        con1: "MEETING DASHBOARD",
-        con2: "Select Timeframe",
-        select1: "Today",
-        select2: "Weekly",
-        select3: "Monthly",
-        msg: "No Record Found",
-        noRecordsFound: true, 
-        currentPath: "/today",
-      });
-    } else {
-      res.render("index", {
-        nav1: "Dashboard",
-        nav2: "Book",
-        con1: "MEETING DASHBOARD",
-        con2: "Select Timeframe",
-        select1: "Today",
-        select2: "Weekly",
-        select3: "Monthly",
-        datas: filteredData,
-        currentPath: "/today",
-      });
-    }
-  } catch (err) {
-    console.log(err);
-  }
-};
-
-exports.today = async (req, res, next) => {
-  try {
-    const datas = await data.getBooking();
-    const startOfDay = new Date(today);
-    const endOfDay = new Date(today);
-    endOfDay.setDate(endOfDay.getDate() + 1);
-
-    const now = new Date(); 
-
-    const filteredData = datas.filter((item) => {
-      return item.startTime >= startOfDay && item.startTime < endOfDay;
-    });
-
-    filteredData.sort((a, b) => a.startTime - b.startTime);
-
+ 
     const currentAndFutureMeetings = filteredData.filter((item) => item.endTime > now);
     const finishedMeetings = filteredData.filter((item) => item.endTime <= now);
-
+ 
     const sortedData = currentAndFutureMeetings.concat(finishedMeetings);
-
-    if (sortedData.length === 0) {
-      res.render("index", {
-        nav1: "Dashboard",
-        nav2: "Book",
-        con1: "MEETING DASHBOARD",
-        con2: "Select Timeframe",
-        select1: "Today",
-        select2: "Weekly",
-        select3: "Monthly",
-        msg: "No Record Found",
-        noRecordsFound: true,
-        currentPath: "/today",
-      });
-    } else {
-      res.render("index", {
-        nav1: "Dashboard",
-        nav2: "Book",
-        con1: "MEETING DASHBOARD",
-        con2: "Select Timeframe",
-        select1: "Today",
-        select2: "Weekly",
-        select3: "Monthly",
-        datas: sortedData,
-        currentPath: "/today",
-      });
-    }
+    res.status(200).send({
+      data: sortedData,
+    })
   } catch (err) {
     console.log(err);
   }
 };
-
-
+ 
 exports.week = async (req, res, next) => {
   try {
     const datas = await data.getBooking();
@@ -208,51 +144,29 @@ exports.week = async (req, res, next) => {
     startOfWeek.setDate(today.getDate() - today.getDay());
     const endOfWeek = new Date(today);
     endOfWeek.setDate(today.getDate() + (6 - today.getDay()) + 1);
-
+ 
     const filteredData = datas.filter((item) => {
       return item.startTime >= startOfWeek && item.startTime < endOfWeek;
     });
-
-    const now = new Date(); 
-
+ 
+    const now = new Date();
+ 
     filteredData.sort((a, b) => a.startTime - b.startTime);
-
+ 
     const currentAndFutureMeetings = filteredData.filter((item) => item.endTime > now);
     const finishedMeetings = filteredData.filter((item) => item.endTime <= now);
-
+ 
     const sortedData = currentAndFutureMeetings.concat(finishedMeetings);
-
-    if (sortedData.length === 0) {
-      res.render("index", {
-        nav1: "Dashboard",
-        nav2: "Book",
-        con1: "MEETING DASHBOARD",
-        con2: "Select Timeframe",
-        select1: "Today",
-        select2: "Weekly",
-        select3: "Monthly",
-        msg: "No Record Found",
-        noRecordsFound: true, 
-        currentPath: "/week",
-      });
-    } else {
-      res.render("index", {
-        nav1: "Dashboard",
-        nav2: "Book",
-        con1: "MEETING DASHBOARD",
-        con2: "Select Timeframe",
-        select1: "Today",
-        select2: "Weekly",
-        select3: "Monthly",
-        datas: sortedData,
-        currentPath: "/week",
-      });
-    }
+ 
+    res.status(200).send({
+      data: sortedData,
+    });
+ 
   } catch (err) {
     console.log(err);
   }
 };
-
+ 
 exports.month = async (req, res, next) => {
   try {
     const datas = await data.getBooking();
@@ -263,107 +177,63 @@ exports.month = async (req, res, next) => {
     const filteredData = datas.filter((item) => {
       return item.startTime >= startOfMonth && item.startTime < endOfMonth;
     });
-
-    const now = new Date(); 
-
+ 
+    const now = new Date();
+ 
     filteredData.sort((a, b) => a.startTime - b.startTime);
-
+ 
     const currentAndFutureMeetings = filteredData.filter((item) => item.endTime > now);
     const finishedMeetings = filteredData.filter((item) => item.endTime <= now);
-
+ 
     const sortedData = currentAndFutureMeetings.concat(finishedMeetings);
-
-    if (sortedData.length === 0) {
-      res.render("index", {
-        nav1: "Dashboard",
-        nav2: "Book",
-        con1: "MEETING DASHBOARD",
-        con2: "Select Timeframe",
-        select1: "Today",
-        select2: "Weekly",
-        select3: "Monthly",
-        msg: "No Record Found",
-        noRecordsFound: true, 
-        currentPath: "/month",
-      });
-    } else {
-      res.render("index", {
-        nav1: "Dashboard",
-        nav2: "Book",
-        con1: "MEETING DASHBOARD",
-        con2: "Select Timeframe",
-        select1: "Today",
-        select2: "Weekly",
-        select3: "Monthly",
-        datas: sortedData,
-        currentPath: "/month",
-      });
-    }
+ 
+    res.status(200).send({
+      data: sortedData,
+    });
+   
   } catch (err) {
     console.log(err);
   }
 };
-
+ 
 exports.all = async (req, res, next) => {
   try {
     const datas = await data.getBooking();
-    const now = new Date(); 
-
+    const now = new Date();
+ 
     datas.sort((a, b) => a.startTime - b.startTime);
-
+ 
     const currentAndFutureMeetings = datas.filter((item) => item.endTime > now);
     const finishedMeetings = datas.filter((item) => item.endTime <= now);
-
+ 
     const sortedData = currentAndFutureMeetings.concat(finishedMeetings);
-    if (sortedData.length === 0) {
-      res.render("index", {
-        nav1: "Dashboard",
-        nav2: "Book",
-        con1: "MEETING DASHBOARD",
-        con2: "Select Timeframe",
-        select1: "Today",
-        select2: "Weekly",
-        select3: "Monthly",
-        msg: "No Record Found",
-        noRecordsFound: true, 
-        currentPath: "/home",
-      });
-    } else {
-      res.render("index", {
-        nav1: "Dashboard",
-        nav2: "Book",
-        con1: "MEETING DASHBOARD",
-        con2: "Select Timeframe",
-        select1: "Today",
-        select2: "Weekly",
-        select3: "Monthly",
-        datas: sortedData,
-        currentPath: "/home",
-      });
-    }
+    res.status(200).send({
+      data: sortedData,
+    });
+ 
   } catch (err) {
     console.log(err);
   }
 };
-
+ 
 exports.delete_entry = async (req, res) => {
   try {
     const meetingId = req.params.id;
     const meetingToDelete = await booking.findOne({ meetingId: meetingId });
-
+ 
     if (!meetingToDelete) {
       return res.status(404).json({ message: "Booking not found" });
     }
-
+ 
     await meetingToDelete.deleteOne({ meetingId: meetingId });
-
+ 
     res.status(200).json({ message: "Booking deleted successfully" });
   } catch (error) {
     console.error("Error deleting booking:", error);
     res.status(500).json({ message: "Internal Server Error" });
   }
 };
-
+ 
 exports.updateBooking = async (req, res) => {
   try {
     const meetingId = req.params.id;
@@ -372,16 +242,16 @@ exports.updateBooking = async (req, res) => {
     const start = moment(startTime);
     const end = moment(endTime);
     const now = moment();
-
+ 
     if (start.isBefore(now) || end.isBefore(now)) {
       return res.status(400).json({ message: "Can't update to past dates and times" });
     }
-
+ 
     const existingBooking = await booking.findOne({ meetingId: meetingId });
     if (!existingBooking) {
       return res.status(404).json({ message: "Meeting not found" });
     }
-
+ 
     const overlappingBooking = await booking.findOne({
       $and: [
         { roomId },
@@ -395,20 +265,20 @@ exports.updateBooking = async (req, res) => {
         },
       ],
     });
-
+ 
     if (overlappingBooking) {
       return res.status(400).json({ message: "Overlapping booking exists" });
     }
-
+ 
     if (end.isBefore(start)) {
       return res.status(400).json({ message: "End time should be after start time" });
     }
-
+ 
     const duration = moment.duration(end.diff(start)).asHours();
     if (duration > 30) {
       return res.status(400).json({ message: "Meeting duration exceeds 30 hours" });
     }
-
+ 
     const updatedBooking = await booking.findOneAndUpdate(
       { meetingId: meetingId },
       {
@@ -421,19 +291,19 @@ exports.updateBooking = async (req, res) => {
       },
       { new: true }
     );
-
+ 
     res.status(200).json(updatedBooking);
   } catch (error) {
     console.error("Error updating booking:", error);
     res.status(500).json({ message: "Internal Server Error" });
   }
 };
-
+ 
 function UTCtolocalFormatter(dateString) {
   const localDateString = new Date(dateString).toLocaleString("en-US", {
     hour12: true,
   });
-
+ 
   const inputDate = new Date(localDateString);
   const year = inputDate.getFullYear();
   const month = (inputDate.getMonth() + 1).toString().padStart(2, "0");
@@ -444,7 +314,7 @@ function UTCtolocalFormatter(dateString) {
   const formattedDateTime = `${year}-${month}-${day}T${hours}:${minutes}:${seconds}`;
   return formattedDateTime;
 }
-
+ 
 exports.getMeetingById = async (req, res) => {
   const meetingId = req.params.id;
   try {
--- index.js
+++ index.js
@@ -3,16 +3,15 @@ const app = express();
 const morgan = require("morgan");
 const dotenv = require("dotenv");
 const dbConfig = require("./Config/mongoDBConfig.js");
+var cors = require('cors');
+
+app.use(cors());
 
 
 const routes = require("./Routes/routes.js");
 
 dotenv.config();
 
-app.set("view engine", "pug"); 
-app.set("views", "C:/Users/USER/Documents/VD_Kalathi/Docs/Scheduler/views");
-
-app.use(express.static("C:/Users/USER/Documents/VD_Kalathi/Docs/Scheduler/public"));
 
 const portNumber = process.env.PORT || 3113;
 
@@ -22,5 +21,8 @@ app.use(express.urlencoded({ extended: true }));
 
 app.use("/", routes) 
 
-app.listen(portNumber, () => console.log(`Node Server run in http://127.0.0.1:${portNumber}/home !`));
+app.get('/',((req,res)=>{
+    res.send('Backend of VD')
+}))
+app.listen(portNumber, () => console.log(`Node Server run in http://127.0.0.1:${portNumber} !`));
 
